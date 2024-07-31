f40f059ba4c3df62e0da25947bacbe61e25d82d8
Gabriel Castro <gabriel.castro@doodle.com>
2024-07-31 16:27:53 +0200

    add react-helmet

---
+++ application/src/App.js
@@ -0,0 +1,28 @@
+import { Helmet } from 'react-helmet';
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Helmet>
+        <meta property="og:title" content="My Title" />
+        <meta property="og:description" content="My Description" />
+        <meta property="og:image" content="https://example.com/image.jpg" />
+        <meta property="og:url" content="https://example.com/page" />
+      </Helmet>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
