8875eb05c24d267fa8abecf493ed67d99db4e812
Gabriel Castro <gabriel.castro@doodle.com>
2024-08-01 14:50:05 +0200

    add redis cache

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,11 @@
 const prerender = require('prerender');
+// const memCache = require('prerender-memory-cache');
+const redisCache = require('./plugins/redisCache');
+
 const server = prerender({
   chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars'],
 });
+// server.use(memCache);
+server.use(redisCache);
+
 server.start();
---
+++ server/src/plugins/redisCache.js
@@ -0,0 +1,56 @@
+const { createClient } = require('redis');
+
+const REDIS_URL = 'redis://redis:6379';
+
+const client = createClient({
+  url: REDIS_URL,
+})
+  .on('error', (err) => {
+    console.error('Redis error:', err);
+  })
+  .on('ready', () => {
+    console.log('Redis ready');
+  })
+  .on('end', () => {
+    console.log('Redis connection closed');
+  });
+
+module.exports = {
+  requestReceived: async (req, res, next) => {
+    const key = req.prerender.url;
+
+    let result;
+    try {
+      await client.connect();
+      result = await client.get(key);
+      await client.disconnect();
+    } catch (err) {
+      console.error('Redis error:', err);
+    }
+
+    if (result) {
+      console.log('Cache hit for', key);
+
+      req.prerender.cacheHit = true;
+      return res.send(200, result);
+    }
+
+    next();
+  },
+  beforeSend: async (req, _res, next) => {
+    if (!req.prerender.cacheHit && req.prerender.statusCode === 200) {
+      const key = req.prerender.url;
+      const value = req.prerender.content;
+
+      try {
+        await client.connect();
+        await client.set(key, value, { EX: 60 });
+        await client.disconnect();
+      } catch (err) {
+        console.error('Redis error:', err);
+      }
+    }
+
+    next();
+  },
+};
